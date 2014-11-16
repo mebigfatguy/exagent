6e7300e290e447c93776241decaefd5db3be5652
Dave Brosius <dbrosius@mebigfatguy.com>
2014-11-15 20:06:37 -0500

    initial project setup

---
+++ src/com/mebigfatguy/exagent/ExAgent.java
@@ -0,0 +1,27 @@
+/*
+ * exagent - An exception stack trace embellisher
+ * Copyright 2014 MeBigFatGuy.com
+ * Copyright 2014 Dave Brosius
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.mebigfatguy.exagent;
+
+import java.lang.instrument.Instrumentation;
+
+public class ExAgent {
+
+    public static void premain(String agentArguments, Instrumentation instrumentation) {
+        
+    }
+}
