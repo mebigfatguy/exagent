bfd4d1dd3a2d368b4d3b68465d2b56809fa10cef
Dave Brosius <dbrosius@mebigfatguy.com>
2014-11-24 00:33:33 -0500

    oi

--- src/com/mebigfatguy/exagent/StackTraceMethodVisitor.java
+++ src/com/mebigfatguy/exagent/StackTraceMethodVisitor.java
@@ -29,7 +29,6 @@ import org.objectweb.asm.AnnotationVisitor;
 import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.Type;
 import org.objectweb.asm.TypePath;
 
 public class StackTraceMethodVisitor extends MethodVisitor {
