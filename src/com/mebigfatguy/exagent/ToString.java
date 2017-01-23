0cb71c230480d0bfcff5d89987850e35a9676ffb
Dave Brosius <dbrosius@mebigfatguy.com>
2017-01-22 20:09:57 -0500

    update cp dates

--- src/com/mebigfatguy/exagent/ToString.java
+++ src/com/mebigfatguy/exagent/ToString.java
@@ -1,7 +1,7 @@
 /*
- * exagent - An exception stack trace embellisher
- * Copyright 2014-2016 MeBigFatGuy.com
- * Copyright 2014-2016 Dave Brosius
+ * baremetal4j - A java aspect for allowing debugging at the byte code level from source debuggers (as in IDEs)
+ * Copyright 2016 MeBigFatGuy.com
+ * Copyright 2016 Dave Brosius
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,70 +27,70 @@ import java.util.Set;
  */
 public class ToString {
 
-	private static class VisitedInfo {
-		Set<Integer> visited = new HashSet<Integer>();
-		int count = 0;
-	}
-	private static final ThreadLocal<VisitedInfo> visited = new ThreadLocal<VisitedInfo>() {
+    private static class VisitedInfo {
+        Set<Integer> visited = new HashSet<>();
+        int count = 0;
+    }
 
-		@Override
-		protected VisitedInfo initialValue() {
-			return new VisitedInfo();
-		}
-	};
-	
-	private ToString() {
-	}
-	
-	public static String build(Object o) {
-		VisitedInfo vi = visited.get();
-		try {
-			vi.count++;
-			return generate(o, vi.visited);
-		} finally {
-			if (--vi.count == 0) {
-				vi.visited.clear();
-			}
-		}
-	}
-	
-	private static String generate(Object o, Set<Integer> visitedObjects) {
-		
-		StringBuilder sb = new StringBuilder(100);
-		Class<?> cls = o.getClass();
-		int identityHC = System.identityHashCode(o);
-		sb.append(cls.getSimpleName()).append('[').append(identityHC).append("]{");
-		
-		if (!visitedObjects.contains(identityHC)) {	
-			try {
-				visitedObjects.add(identityHC);
-				String sep = "";
-				for (Field f : cls.getDeclaredFields()) {
-					if (!f.isSynthetic() && !f.getName().contains("$")) {
-						sb.append(sep);
-						sep = ", ";
-						sb.append(f.getName()).append('=');
-						try {
-		    				f.setAccessible(true);
-		    				Object value = f.get(o);
-		    				if (value == null) {
-		    					sb.append((String) null);
-		    				} else if (value.getClass().isArray()) {
-		    					sb.append(Arrays.toString((Object[]) value));
-		    				} else {
-		    					sb.append(value);
-		    				}
-						} catch (SecurityException e) {
-						    sb.append("*SECURITY_EXCEPTION*");
-						}
-					}
-				}
-			} catch (Exception e) {
-			}
-		}
-		
-		sb.append('}');
-		return sb.toString();
-	}
+    private static final ThreadLocal<VisitedInfo> visited = new ThreadLocal<VisitedInfo>() {
+
+        @Override
+        protected VisitedInfo initialValue() {
+            return new VisitedInfo();
+        }
+    };
+
+    private ToString() {
+    }
+
+    public static String build(Object o) {
+        VisitedInfo vi = visited.get();
+        try {
+            vi.count++;
+            return generate(o, vi.visited);
+        } finally {
+            if (--vi.count == 0) {
+                vi.visited.clear();
+            }
+        }
+    }
+
+    private static String generate(Object o, Set<Integer> visitedObjects) {
+
+        StringBuilder sb = new StringBuilder(100);
+        Class<?> cls = o.getClass();
+        int identityHC = System.identityHashCode(o);
+        sb.append(cls.getSimpleName()).append('[').append(identityHC).append("]{");
+
+        if (!visitedObjects.contains(identityHC)) {
+            try {
+                visitedObjects.add(identityHC);
+                String sep = "";
+                for (Field f : cls.getDeclaredFields()) {
+                    if (!f.isSynthetic() && !f.getName().contains("$")) {
+                        sb.append(sep);
+                        sep = ", ";
+                        sb.append(f.getName()).append('=');
+                        try {
+                            f.setAccessible(true);
+                            Object value = f.get(o);
+                            if (value == null) {
+                                sb.append((String) null);
+                            } else if (value.getClass().isArray()) {
+                                sb.append(Arrays.toString((Object[]) value));
+                            } else {
+                                sb.append(value);
+                            }
+                        } catch (SecurityException e) {
+                            sb.append("*SECURITY_EXCEPTION*");
+                        }
+                    }
+                }
+            } catch (Exception e) {
+            }
+        }
+
+        sb.append('}');
+        return sb.toString();
+    }
 }
-
